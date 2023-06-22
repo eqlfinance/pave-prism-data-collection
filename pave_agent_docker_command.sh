159532a5fea7db506a24203b714397a555a9eb07
Langston Howley <ljhowley11@gmail.com>
2023-06-22 13:17:27 -0400

    Set up pave agent architecture

---
+++ pave_agent_docker_command.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Legacy, keep here if we need to use Pave DB Connector
+#sudo docker run -v /home/langston/pave-prism/pave-agent/pa-server-ca.pem:/pa-server-ca.pem -v /home/langston/pave-prism/pave-agent/pa-client-cert.pem:/pa-client-cert.pem -v /home/langston/pave-prism/pave-agent/pa-client-key.pem:/pa-client-key.pem --env-file /home/langston/pave-prism/pave-agent/.env -p 8123:8000 docker.io/pavedev/agent:latest
+
+sudo docker kill pave-agent
+sudo docker rm pave-agent
+sudo docker run -d --env-file /home/langston/pave-prism/pave-agent.env -p 8123:8000 docker.io/pavedev/agent:latest
