5d3b284cd69facb1766bab05d0787dcd0b1e75a2
Langston Howley <ljhowley11@gmail.com>
2023-06-23 11:12:51 -0400

    make transaction filler rate limit aware

--- transaction_filler.py
+++ transaction_filler.py
@@ -6,8 +6,10 @@ rows = conn.execute(
     "SELECT DISTINCT access_token, user_id FROM public.plaid_links WHERE status = 'active'"
 ).fetchall()
 
-for row in tqdm(rows):
-    row = row._asdict()
+timer = 0
+iter_counter = 0
+for i in tqdm(len(rows)):
+    row = rows[i]._asdict()
     access_token, user_id = decrypt(row["access_token"]), str(row["user_id"])
     time_in_days = 365 * 4
 
@@ -19,3 +21,10 @@ for row in tqdm(rows):
     )
     pave_agent_end = datetime.datetime.now()
     print(f"  Pave Agent res code: {res.status_code}, took {pave_agent_end-pave_agent_start}\n        {res.json()}")
+    timer += (pave_agent_end-pave_agent_start).seconds
+    iter_counter += 1
+
+    if iter_counter == 1000:
+        time.sleep(60-timer if timer < 60 else 0)
+        timer = 0
+        iter_counter = 0
